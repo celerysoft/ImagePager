16e71cc2623e624511d7f08e5530df7673edf82c
Celery Qin <celerysoft@gmail.com>
2015-11-21 11:28:11 +0800

    update ImageUtil

--- library/src/main/java/com/celerysoft/imagepager/util/ImageUtil.java
+++ library/src/main/java/com/celerysoft/imagepager/util/ImageUtil.java
@@ -3,17 +3,31 @@ package com.celerysoft.imagepager.util;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import java.io.File;
 
 /**
  * Created by Administrator on 2015-11-20.
  */
 public class ImageUtil {
+
+    private static final String TAG = ImageUtil.class.getSimpleName();
+
     /**
      * get bitmap by file absolute path
-     * @param fileName
-     * @return
+     * @param filePath file absolute path
+     * @return bitmap
      */
-    public static final Bitmap getBitmap(Context context, String fileName) {
+    public static final Bitmap getBitmap(Context context, String filePath) {
+        File f = new File(filePath);
+        if (f != null) {
+            if (!f.exists()) {
+                Log.w(TAG, "No such file: " + filePath);
+                return null;
+            }
+        }
+
         Bitmap bitmap = null;
         try {
             int targetWidth = (int) (context.getResources().getDisplayMetrics().widthPixels * 0.8);
@@ -21,7 +35,7 @@ public class ImageUtil {
 
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(fileName, options);
+            BitmapFactory.decodeFile(filePath, options);
 
             int photoWidth = options.outWidth;
             int photoHeight = options.outHeight;
@@ -32,7 +46,7 @@ public class ImageUtil {
                     (double) options.outWidth / 1024f,
                     (double) options.outHeight / 1024f)));
             options.inJustDecodeBounds = false;
-            bitmap = BitmapFactory.decodeFile(fileName, options);
+            bitmap = BitmapFactory.decodeFile(filePath, options);
         } catch (OutOfMemoryError error) {
             error.printStackTrace();
         }
